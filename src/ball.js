e4c18c591fc95cdb4def29f93c071a5c4f73f0cd
Jacky Tang <jackytck@gmail.com>
2017-08-14 00:09:40 +0800

    :hankey: spring animated ball

--- src/ball.js
+++ src/ball.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
+import {
+  Animated,
+  View
+} from 'react-native'
 
 class Ball extends Component {
+  componentWillMount () {
+    this.position = new Animated.ValueXY(0, 0)
+    Animated.spring(this.position, {
+      toValue: { x: 200, y: 500 },
+      tension: 10
+    }).start()
+  }
+
   render () {
     return (
-      <View style={styles.ball} />
+      <Animated.View style={this.position.getLayout()}>
+        <View style={styles.ball} />
+      </Animated.View>
     )
   }
 }
