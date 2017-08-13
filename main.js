584a9a59181221c5509bcf62a814492893f115ba
Jacky Tang <jackytck@gmail.com>
2017-08-13 14:24:54 +0800

    :tada: initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+import Expo from 'expo'
+import React from 'react'
+import {
+  StyleSheet,
+  View
+} from 'react-native'
+import Ball from './src/ball'
+
+class App extends React.Component {
+  render () {
+    return (
+      <View style={styles.container}>
+        <Ball />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
+
+Expo.registerRootComponent(App)
