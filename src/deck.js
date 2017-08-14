36c656d997665fdb1a0533ef9205ab0bcab95ea7
Jacky Tang <jackytck@gmail.com>
2017-08-15 00:05:13 +0800

    :hankey: WIP Deck component

---
+++ src/deck.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import {
+  Animated,
+  View
+} from 'react-native'
+
+class Deck extends Component {
+  render () {
+    return (
+      <View />
+    )
+  }
+}
+
+export default Deck
