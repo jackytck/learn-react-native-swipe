0b96d42b689ce0758594a6e3e5ebd36d33e70ae1
Jacky Tang <jackytck@gmail.com>
2017-08-27 00:07:05 +0800

    :hammer: stack up all cards in absolute position

--- src/deck.js
+++ src/deck.js
@@ -92,15 +92,19 @@ class Deck extends Component {
         return (
           <Animated.View
             key={item.id}
-            style={this.getCardStyle()}
+            style={[this.getCardStyle(), styles.cardStyle]}
             {...this.state.panResponder.panHandlers}
           >
             {this.props.renderCard(item)}
           </Animated.View>
         )
       }
-      return this.props.renderCard(item)
-    })
+      return (
+        <View key={item.id} style={styles.cardStyle}>
+          {this.props.renderCard(item)}
+        </View>
+      )
+    }).reverse()
   }
 
   render () {
@@ -118,6 +122,13 @@ Deck.propTypes = {
   onSwipeLeft: PropTypes.func,
   onSwipeRight: PropTypes.func,
   renderNoMoreCards: PropTypes.func
+}
+
+const styles = {
+  cardStyle: {
+    position: 'absolute',
+    width: SCREEN_WIDTH
+  }
 }
 
 export default Deck
